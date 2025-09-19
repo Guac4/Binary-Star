3b8bb62f3309633bc5354f220d0a552fa8afeb39
MUKILTEO\1725468 <ceballosg365@gmail.com>
2025-09-19 08:39:18 -0700

    Fixed ground stuck

--- 2D Template/Assets/OxygenSystem/OxygenLung/OxygenTank.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Oxygen System/Oxygen Lung")]
-public class OxygenTank : ScriptableObject
-{
-    public float capacity = 75f;
-    public float regenRate = 1f;
-    public float depletionModifier = 1f;
-}
